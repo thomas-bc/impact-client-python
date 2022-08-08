0936f398c44018300f6d9b2805ff82d09467b48f
abhilash_kumar_nair <abhilash.kumar@modelon.com>
2022-08-08 06:23:58 +0000

    f

--- modelon/impact/client/sal/model_executable.py
+++ modelon/impact/client/sal/model_executable.py
@@ -1,5 +1,5 @@
 """Model executable service module"""
-from typing import Any, Dict, Tuple
+from typing import Any, Dict
 from modelon.impact.client.sal.http import HTTPClient
 from modelon.impact.client.sal.uri import URI
 
